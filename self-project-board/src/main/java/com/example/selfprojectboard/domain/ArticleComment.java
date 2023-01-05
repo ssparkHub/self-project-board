993a85caf987d48ae23fccf3d6f4ded3415caa7e
sspark <tjdtnf110@naver.com>
2023-01-05 15:46:11 +0900

    #8 도메인 설정

--- self-project-board/src/main/java/com/example/selfprojectboard/domain/ArticleComment.java
+++ self-project-board/src/main/java/com/example/selfprojectboard/domain/ArticleComment.java
@@ -5,15 +5,15 @@ import java.time.LocalDateTime;
 public class ArticleComment {
 
     private Long id;
-    private Article article;
-    private String title;
-    private String content;
-    private String hashtag;
+    private Article article; // 게시글(ID)
+    private String title; // 제목
+    private String content; // 내용
+    private String hashtag; // 해시태그
 
-    private LocalDateTime createAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
 
 
 }
