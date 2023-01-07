25a3df78a4849c80fcf2277fd49de9a0e357c69e
sspark <tjdtnf110@naver.com>
2023-01-07 21:29:37 +0900

    #Test-Case + 의존성 업데이트 - JPA + MySQL + H2
    Spring Boot에 DB와 JPA 의존성 추가

--- self-project-board/src/main/java/com/example/selfprojectboard/domain/Article.java
+++ self-project-board/src/main/java/com/example/selfprojectboard/domain/Article.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
@@ -22,6 +23,7 @@ import java.util.Set;
         @Index(columnList = "createAt"),
         @Index(columnList = "createdBy")
 })
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Article {
 
--- self-project-board/src/main/java/com/example/selfprojectboard/domain/ArticleComment.java
+++ self-project-board/src/main/java/com/example/selfprojectboard/domain/ArticleComment.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -20,6 +21,7 @@ import java.util.Objects;
         @Index(columnList = "createdBy")
 
 })
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class ArticleComment {
 
