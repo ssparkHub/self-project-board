c0f19301331e05ba06c9cade9d08d06517314092
sspark <tjdtnf110@naver.com>
2023-01-19 00:53:40 +0900

    #38 - Hashtag 검색 기능 테스트 작성
    - 테스트를 위한 Test메소드 작성
    - Querydsl을 사용하기위해
     ArticleRepositoryCustom 인터페이스 및 ArticleRepositoryCustomImpl 클래스 생성
     -->Impl객체는 QuerydslRepositorySupport을 상속받고 ~Custom을 구현한다
     (>>Querydsl을 사용하기 위한 약속)

---
+++ self-project-board/src/main/java/com/example/selfprojectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,27 @@
+package com.example.selfprojectboard.repository.querydsl;
+
+import com.example.selfprojectboard.domain.Article;
+import com.example.selfprojectboard.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom { //interface name + Impl은 약속이다
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull())
+                .fetch();
+
+    }
+}
