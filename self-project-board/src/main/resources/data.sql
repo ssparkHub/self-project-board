c74bec8cf21d74752623ca59646206e7e2504ae5
sspark <tjdtnf110@naver.com>
2023-01-27 16:52:10 +0900

    #34 - 패스워드 인코더가 인식할 수 있게 패스워드 정보 수정 앞머리에 '{encoding-info}'와 같이 암호화 정보를 넣어줘야 패스워드 인코더가 읽을 수 있다. > 이에 패스워드 데이터를 알맞게 수정함

--- self-project-board/src/main/resources/data.sql
+++ self-project-board/src/main/resources/data.sql
@@ -2,11 +2,11 @@
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at,
                           modified_by)
-values ('sspark', 'asdf1234', 'Sspark', 'sspark@mail.com', 'I am Sspark.', now(), 'sspark', now(), 'sspark')
+values ('sspark', '{noop}asdf1234', 'Sspark', 'sspark@mail.com', 'I am Sspark.', now(), 'sspark', now(), 'sspark')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at,
                           modified_by)
-values ('sspark2', 'asdf1234', 'Sspark2', 'sspark2@mail.com', 'I am Sspark2.', now(), 'sspark2', now(), 'sspark2')
+values ('sspark2', '{noop}asdf1234', 'Sspark2', 'sspark2@mail.com', 'I am Sspark2.', now(), 'sspark2', now(), 'sspark2')
 ;
 
 -- 123 게시글
